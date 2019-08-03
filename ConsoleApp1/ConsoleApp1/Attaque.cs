4d136294df4be504fab8342de1d9997bbc0d5a26
xXImprimanteXx <xXImprimanteXx@gmail.com>
2019-08-03 17:32:40 +0200

    jefédestrucq

--- ConsoleApp1/ConsoleApp1/Attaque.cs
+++ ConsoleApp1/ConsoleApp1/Attaque.cs
@@ -9,10 +9,19 @@ namespace ConsoleApp1
     //comment pls
     class Attaque
     {
+        public string nom { get; private set; }
         public PokemonType Type { get ; private set; }
         public int Effet { get; private set; }
         public int Precision { get; private set; }
         public int Damage { get; private set; }
+
+        public Attaque(string p_nom, PokemonType p_type,int p_damage,int p_precision)
+        {
+            this.nom = p_nom;
+            this.Type = p_type;
+            this.Damage = p_damage;
+            this.Precision = p_precision;
+        }
 
     }
 }
